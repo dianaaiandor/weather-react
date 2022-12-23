6270a0f7926e814c7bd3c60ba868b22948fa451c
dianaaiandor <110303967+dianaaiandor@users.noreply.github.com>
2022-12-23 21:48:42 +0100

    moved 3 components from Codesandbox to VS project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
-import Weather from "./weather";
+import TodaysWeather from "./TodaysWeather";
+import Date from "./Date";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello everyone</h1>
-        <Weather />
-      </header>
+      <Date />
+      <Search name="Kyiv" />
+      <TodaysWeather
+        description="Clouds"
+        wind={12}
+        humidity={80}
+        temperature={20}
+        feeling={18}
+      />
     </div>
   );
 }
---
+++ src/Date.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Date.css";
+
+export default function Date() {
+  return <div className="Date">Sunday, 21:26 (GMT+2) </div>;
+}
